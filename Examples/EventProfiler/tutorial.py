4587045fac9ec0540ea2d8c31c2eb9eac79c1a1f
Snail Mail <>
2016-12-25 23:45:40 +0200

    Vectorize find_events using DataFrame operators. Faster and cleaner.

--- Examples/EventProfiler/tutorial.py
+++ Examples/EventProfiler/tutorial.py
@@ -41,34 +41,10 @@ nan = no information about any event.
 
 def find_events(ls_symbols, d_data):
     ''' Finding the event dataframe '''
-    df_close = d_data['close']
-    ts_market = df_close['SPY']
-
     print "Finding Events"
 
-    # Creating an empty dataframe
-    df_events = copy.deepcopy(df_close)
-    df_events = df_events * np.NAN
-
-    # Time stamps for the event range
-    ldt_timestamps = df_close.index
-
-    for s_sym in ls_symbols:
-        for i in range(1, len(ldt_timestamps)):
-            # Calculating the returns for this timestamp
-            f_symprice_today = df_close[s_sym].ix[ldt_timestamps[i]]
-            f_symprice_yest = df_close[s_sym].ix[ldt_timestamps[i - 1]]
-            f_marketprice_today = ts_market.ix[ldt_timestamps[i]]
-            f_marketprice_yest = ts_market.ix[ldt_timestamps[i - 1]]
-            f_symreturn_today = (f_symprice_today / f_symprice_yest) - 1
-            f_marketreturn_today = (f_marketprice_today / f_marketprice_yest) - 1
-
-            # Event is found if the symbol is down more then 3% while the
-            # market is up more then 2%
-            if f_symreturn_today <= -0.03 and f_marketreturn_today >= 0.02:
-                df_events[s_sym].ix[ldt_timestamps[i]] = 1
-
-    return df_events
+    pct_change = d_data['close'].pct_change().iloc[1:]
+    return (pct_change <= -0.03).__and__(pct_change.SPY >= 0.02, 'rows')
 
 
 if __name__ == '__main__':
@@ -80,7 +56,7 @@ if __name__ == '__main__':
     ls_symbols = dataobj.get_symbols_from_list('sp5002012')
     ls_symbols.append('SPY')
 
-    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+    ls_keys = ['close']
     ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
 
